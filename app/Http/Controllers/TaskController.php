e0835249ee7ce068ff5eb864641a3cef907c6ba4
paingsoeko <social.kopaing@gmail.com>
2024-01-10 11:56:21 +0630

    initial commit

---
+++ app/Http/Controllers/TaskController.php
@@ -0,0 +1,49 @@
+<?php
+
+namespace App\Http\Controllers;
+
+use App\Http\Requests\StoreTaskRequest;
+use App\Http\Requests\UpdateTaskRequest;
+use App\Http\Resources\TaskCollection;
+use App\Http\Resources\TaskResource;
+use App\Models\Task;
+use Illuminate\Http\Request;
+
+class TaskController extends Controller
+{
+    public function index(Request $request)
+    {
+        return new TaskCollection(Task::paginate());
+//        return response()->json(Task::all());
+    }
+
+    public function show(Request $request, Task $task)
+    {
+        return new TaskResource($task);
+    }
+
+    public function store(StoreTaskRequest $request)
+    {
+        $validate = $request->validated();
+
+        $task = Task::create($validate);
+
+        return new TaskResource($task);
+    }
+
+    public function update(UpdateTaskRequest $request, Task $task)
+    {
+        $validated = $request->validated();
+
+        $task->update($validated);
+
+        return new TaskResource($task);
+    }
+
+    public function destroy(Request $request, Task $task)
+    {
+        $task->delete();
+
+        return response()->noContent();
+    }
+}
